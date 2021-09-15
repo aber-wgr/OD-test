77cc452583d2ae2ad6480de77cf47390a4baf874
aber-wgr <wgr@aber.ac.uk>
2021-09-15 09:47:36 +0100

    Trying a different way

--- models/pixelcnn/model.py
+++ models/pixelcnn/model.py
@@ -1,8 +1,8 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
-import models.pixelcnn.layers
-import models.pixelcnn.utils
+from models.pixelcnn.layers import *
+from models.pixelcnn.utils import *
 import numpy as np
 
 class PixelCNNLayer_up(nn.Module):
